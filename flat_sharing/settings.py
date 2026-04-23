5cf5eb9ff1cc9dc9b0f89321287902ab18cfda13
asifikbal09 <asif.ikbal662@gmail.com>
2026-04-24 01:11:27 +0600

    connect realway database

--- flat_sharing/settings.py
+++ flat_sharing/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'flat_sharing.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flat_sharing',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
